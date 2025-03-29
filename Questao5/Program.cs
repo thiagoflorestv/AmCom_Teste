5b68a47532c0bbc4cfa9e5166b6588df643d3e99
thiago.rosa <thiago.rosa@agenciaopen.com>
2025-03-29 03:10:15 -0300

    Configuração SwaggerUI para rodar localhost

--- Questao5/Program.cs
+++ Questao5/Program.cs
@@ -39,11 +39,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API - Movimentação de Conta Corrente v1");
-        c.RoutePrefix = string.Empty;
-    });
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
